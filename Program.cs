bd240e71704e505b3106b5750b257b2492225f7f
VoQuocViet4323 <140177566+VoQuocViet4323@users.noreply.github.com>
2024-10-06 23:14:01 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int choice = -1;
+        while (choice != 0)
+        {
+            Console.WriteLine("Menu");
+            Console.WriteLine("1. Ve hinh tam giac");
+            Console.WriteLine("2. Ve hinh vuong");
+            Console.WriteLine("3. Ve hinh chu nhat");
+            Console.WriteLine("0. Thoat");
+            Console.WriteLine("Nhap lua chon: ");
+            choice = Int32.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Hinh tam giac");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * *");
+                    Console.WriteLine("* * * *");
+                    Console.WriteLine("* * *");
+                    Console.WriteLine("* *");
+                    Console.WriteLine("*");
+                    break;
+                case 2:
+                    Console.WriteLine("Hinh vuong");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    break;
+                case 3:
+                    Console.WriteLine("Hinh chu nhat");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    break;
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("No choice!");
+                    break;
+            }
+
+        }
+    }
+}
